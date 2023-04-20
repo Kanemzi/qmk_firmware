08e7d0a9fe4f35bd852f3bfe0ee5a3ff69136466
SimonROZEC <simonrozec44@gmail.com>
2023-04-20 22:58:20 +0200

    Start writing the midi game logic

--- keyboards/planck/keymaps/kanemzi/layers/layer_midi.c
+++ keyboards/planck/keymaps/kanemzi/layers/layer_midi.c
@@ -17,7 +17,8 @@ typedef struct
 {
 	bool started : 1;
     bool player_turn: 1;
-    uint16_t timer; // multi purpose timer (used for delays and timing note batches)
+	bool is_replaying: 1; // set to 1 if currently replaying a batch
+    uint16_t play_start_time; // used for multi purpose timing (waiting and timing note batches)
 
     uint16_t current_notes_batch[MI_GAME_BATCH_MAX_LENGTH];
     bool current_is_simultaneous:1;
@@ -25,39 +26,10 @@ typedef struct
     uint8_t wrong_inputs[MI_GAME_MAX_MISTAKES];
     uint8_t correct_inputs[MI_GAME_BATCH_MAX_LENGTH];
 
-    uint8_t
     midi_game_config_t config;
 } midi_game_data_t;
 
 static midi_game_data_t midi_game;
-
-static void _start_midi_game(void)
-{
-	if (midi_game.started) return;
-	midi_game.started = true;
-    srand(timer_read32());
-    tap_code16(MI_OC2); // reset octave
-}
-
-static void _stop_midi_game(void)
-{
-	if (!midi_game.started) return;
-	midi_game.started = false;
-
-}
-
-static void _update_midi_game(void)
-{
-    if (!midi_game.started) return;
-
-
-}
-
-static bool _on_process_record_midi_game(uint16_t keycode, keyrecord_t *record)
-{
-    // @note: don't count correct note again if replaying it
-    return false;
-}
 
 // fills the buffer passed in parameter with a random note batch based on the current game config
 static void _generate_random_note_batch(uint16_t* out_batch_buffer, bool* out_is_simultaneous)
@@ -71,7 +43,97 @@ static void _generate_random_note_batch(uint16_t* out_batch_buffer, bool* out_is
 
 static uint8_t _get_led_index_from_midi_note(uint16_t midi_keycode)
 {
-    if (midi_keycode < MI_C || midi_keycode > MI_B2) return 0xff;
+    if (midi_keycode < MI_C || midi_keycode > MI_B2) return UINT8_MAX;
+	// @todo
+}
+
+static void _start_midi_game(void)
+{
+	if (midi_game.started) return;
+	midi_game.started = true;
+    srand(timer_read32());
+
+    tap_code16(MI_OC2); // reset octave
+	tap_code16(QK_MIDI_ALL_NOTES_OFF);
+}
+
+static void _stop_midi_game(void)
+{
+	if (!midi_game.started) return;
+	midi_game.started = false;
+}
+
+static void _update_midi_game(uint16_t current_time)
+{
+    if (!midi_game.started) return;
+
+	if (!midi_game.player_turn) // Wait and play the current note batch
+	{
+		uint16_t play_elapsed_time = TIMER_DIFF_16(current_time, midi_game.play_start_time);
+		if (!midi_game.is_replaying)
+		{
+			// Wait a bit before playing the batch (if replay requested, just start directly)
+			if (play_elapsed_time < MI_GAME_DELAY_BETWEEN_BATCHES_MS) return;
+			else play_elasped_time -= MI_GAME_DELAY_BETWEEN_BATCHES_MS;
+		}
+
+
+	}
+}
+
+static void _play_notes_batch(bool replay)
+{
+	midi_game.player_turn = false;
+	midi_game.timer = 0;
+	midi_game.is_replaying = replay;
+	midi_game.play_start_time = timer_read();
+
+	if (!replay) // If not replaying, start a new round
+	{
+		_generate_random_note_batch(&midi_game.current_notes_batch, &midi_game.current_is_simultaneous);
+		memset(wrong_inputs, UINT8_MAX, sizeof(wrong_inputs));
+    	memset(correct_inputs, UINT8_MAX, sizeof(correct_inputs));
+	}
+
+	tap_code16(QK_MIDI_ALL_NOTES_OFF);
+}
+
+static bool _on_process_record_midi_game(uint16_t keycode, keyrecord_t *record)
+{
+	if (!midi_game.started) return true;
+    // @note: don't count correct note again if replaying it
+
+	switch (keycode)
+	{
+		case MI_OCN2 ... MI_OCTU:
+			if (midi_game.started) return false; // Forbid octave variations during the game
+			break;
+		case KZ_MI_GAME_REPLAY:
+			if (record->event.pressed) _play_notes_batch(true);
+			return false;
+		case KZ_MI_GAME_NEXT:
+			if (record->event.pressed) _play_notes_batch(false);
+			return false;
+	}
+
+    return true;
+}
+
+static void _render_midi_game(uint16_t current_time, uint8_t led_min, uint8_t led_max)
+{
+	if (!midi_game.started) return;
+
+	if (!midi_game.player_turn)
+	{
+		for (uint8_t i = 38; i < 45; i++)
+		{
+			RGB_MATRIX_INDICATOR_SET_COLOR(i, 0, 255, 255);
+		}
+	}
+
+	RGB_MATRIX_INDICATOR_SET_COLOR(45, 255, 0, 0); // Midi Game status led
+	RGB_MATRIX_INDICATOR_SET_COLOR(36, 255, 128, 0); // Replay button
+	RGB_MATRIX_INDICATOR_SET_COLOR(37, 255, 255, 0); // Next button
 }
 
 #pragma endregion MIDI_GAME
@@ -107,23 +169,22 @@ void on_layer_render_midi(uint8_t led_min, uint8_t led_max)
 	{
         // Update the Midi Game state before renreding
         // @todo could be done in an on_update callback
-        _update_midi_game();
+        _update_midi_game(now);
 		// Midi Game rendering logic
-		RGB_MATRIX_INDICATOR_SET_COLOR(45, 255, 0, 0);
+		_render_midi_game(now, led_min, led_max);
 	}
 	else if (midi_game_key_press_time > 0 && TIMER_DIFF_16(now, midi_game_key_press_time) >= MI_GAME_KEY_HOLD_TIME_MS)
 	{
-		RGB_MATRIX_INDICATOR_SET_COLOR(45, 255, 255, 0);
+		RGB_MATRIX_INDICATOR_SET_COLOR(45, 255, 255, 0); // Midi Game button confirm
 	}
 }
 
 bool on_process_record_midi(uint16_t keycode, keyrecord_t *record)
 {
+	if (midi_game.started && !_on_process_record_midi_game(keycode, record)) return false;
+
 	switch (keycode)
 	{
-        case MI_OCN2 ... MI_OCTU:
-            if (midi_game.started) return false; // Forbid octave variations during the game
-            break;
 		case KZ_MI_GAME:
 			if (!midi_game.started) // needs to hold the key to start game (to avoid missclicks)
 			{
